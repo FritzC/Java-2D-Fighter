a197f7fe9bb3f89380fe92acd510d45268753062
FritzC <ownerfritz@gmail.com>
2016-04-13 13:45:13 -0500

    Added and completed Animation and AnimationStep panels

--- src/fighter/editor/Editor.java
+++ src/fighter/editor/Editor.java
@@ -7,6 +7,7 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -38,8 +39,21 @@ public class Editor {
 	private static JButton newAnimation;
 	private static JButton deleteAnimation;
 	private static JTextField animationName;
-	private static JSpinner fighterPositionX;
-	private static JSpinner fighterPositionY;
+	private static JSpinner ecbTopLeftX;
+	private static JSpinner ecbTopLeftY;
+	private static JSpinner ecbBottomRightX;
+	private static JSpinner ecbBottomRightY;
+	private static JSpinner centerY;
+
+	private static JSpinner stepSelecter;
+	private static JButton newStep;
+	private static JButton deleteStep;
+	private static JButton duplicateStep;
+	private static JButton setSprite;
+	private static JSpinner stepDuration;
+	private static JCheckBox stepMovesSelf;
+	private static JSpinner stepVelocityX;
+	private static JSpinner stepVelocityY;
 
 	public static void main(String args[]) {
 		JFrame frame = new JFrame();
@@ -69,7 +83,7 @@ public class Editor {
 		editPanel.setLayout(new GridLayout(1, 4));
 		initFighterPanel();
 		initAnimationsPanel();
-		animationStepPanel = new JPanel();
+		initAnimationStepsPanel();
 		collisionBoxPanel = new JPanel();
 		editPanel.add(fighterPanel);
 		editPanel.add(animationsPanel);
@@ -98,10 +112,15 @@ public class Editor {
 		deleteAnimation = new JButton("Delete");
 		animationName = new JTextField();
 		JPanel position = new JPanel(new GridLayout(0, 2));
-		fighterPositionX = new JSpinner(new SpinnerNumberModel(1, -10, 10, 0.05));
-		fighterPositionY = new JSpinner(new SpinnerNumberModel(1, -10, 10, 0.05));
-		position.add(fighterPositionX);
-		position.add(fighterPositionY);
+		ecbTopLeftX = new JSpinner(new SpinnerNumberModel(0, -2, 2, 0.05));
+		ecbTopLeftY = new JSpinner(new SpinnerNumberModel(1, -2, 2, 0.05));
+		ecbBottomRightX = new JSpinner(new SpinnerNumberModel(1, -2, 2, 0.05));
+		ecbBottomRightY = new JSpinner(new SpinnerNumberModel(0, -2, 2, 0.05));
+		position.add(ecbTopLeftX);
+		position.add(ecbTopLeftY);
+		position.add(ecbBottomRightX);
+		position.add(ecbBottomRightY);
+		centerY = new JSpinner(new SpinnerNumberModel(0.5, -2, 2, 0.05));
 		
 		animationsPanel.add(new JLabel("Animation:", SwingConstants.CENTER));
 		animationsPanel.add(animationSelector);
@@ -109,8 +128,40 @@ public class Editor {
 		animationsPanel.add(deleteAnimation);
 		animationsPanel.add(new JLabel("Animation Name:", SwingConstants.CENTER));
 		animationsPanel.add(animationName);
-		animationsPanel.add(new JLabel("Ground Point:", SwingConstants.CENTER));
+		animationsPanel.add(new JLabel("ECB:", SwingConstants.CENTER));
 		animationsPanel.add(position);
+		animationsPanel.add(new JLabel("Center:", SwingConstants.CENTER));
+		animationsPanel.add(centerY);
+	}
+	
+	public static void initAnimationStepsPanel() {
+		animationStepPanel = new JPanel(new GridLayout(0, 2));
+		stepSelecter = new JSpinner(new SpinnerNumberModel(0, 0, 1, 1));
+		JPanel addRemove = new JPanel(new GridLayout(0, 2));
+		newStep = new JButton("+");
+		deleteStep = new JButton("-");
+		addRemove.add(newStep);
+		addRemove.add(deleteStep);
+		duplicateStep = new JButton("Copy");
+		setSprite = new JButton("Choose...");
+		stepDuration = new JSpinner(new SpinnerNumberModel(1, 1, 100, 1));
+		stepMovesSelf = new JCheckBox("   Velocity:");
+		JPanel velocity = new JPanel(new GridLayout(0, 2));
+		stepVelocityX = new JSpinner(new SpinnerNumberModel(0, -100, 100, 0.05));
+		stepVelocityY = new JSpinner(new SpinnerNumberModel(0, -100, 100, 0.05));
+		velocity.add(stepVelocityX);
+		velocity.add(stepVelocityY);
+		
+		animationStepPanel.add(new JLabel("Step:", SwingConstants.CENTER));
+		animationStepPanel.add(stepSelecter);
+		animationStepPanel.add(addRemove);
+		animationStepPanel.add(duplicateStep);
+		animationStepPanel.add(new JLabel("Sprite:", SwingConstants.CENTER));
+		animationStepPanel.add(setSprite);
+		animationStepPanel.add(new JLabel("Duration:", SwingConstants.CENTER));
+		animationStepPanel.add(stepDuration);
+		animationStepPanel.add(stepMovesSelf);
+		animationStepPanel.add(velocity);
 	}
 
 }
